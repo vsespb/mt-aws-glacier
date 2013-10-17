ee59708aa11ead3296ab4b80f2313317c7e2e37c
Victor <victor@vsespb.ru>
2013-10-17 13:30:54 +0400

    fix scripts/ to be more reliable

--- scripts/build_debian.sh
+++ scripts/build_debian.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+cd .. && grep -s -q APP-MTAWS-ROOT-DIR Build.PL || exit 1 # protect from wrong dir
 
 DISTROS="precise quantal raring saucy"
 for DISTRO in $DISTROS
 do
 rm -rf debian
 mkdir -p debian/source
-scripts/write_debian_files.pl packaging $DISTRO
+scripts/write_debian_files.pl ./packaging $DISTRO
 debuild -S
 rm -rf debian
 done
