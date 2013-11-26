ca40c250f9ac7dea7fa7b465eb74f95968b82d1f
Victor <victor@vsespb.ru>
2013-11-26 17:15:48 +0400

    coverage script now accept parameters

--- scripts/test_cover.sh
+++ scripts/test_cover.sh
@@ -2,7 +2,13 @@
 set -e
 COVER_DB=/tmp/cover_db
 cover -delete $COVER_DB
-#MT_COVER=-MDevel::Cover=-db,$COVER_DB ../test.t cover
-perl -MDevel::Cover=-db,$COVER_DB ../t/unit/queue_job/fetch_and_download_inventory.t
+
+if [ -z "$1" ]
+then
+MT_COVER=-MDevel::Cover=-db,$COVER_DB ../test.t cover
+else
+perl -MDevel::Cover=-db,$COVER_DB $(find ../t/ |grep -E "\.t$" |grep $1 )
+fi
+
 cover $COVER_DB -report=html -launch
 echo OK DONE
