0085702fd43bbe4ad1083911122e5f79485f62c0
Victor <victor@vsespb.ru>
2013-12-07 15:28:33 +0400

    fsm test

--- t/gemu-test/t.sh
+++ t/gemu-test/t.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-./gemu-test.pl |USENEWFSM=0 ./gemu-test-worker.pl -n 4  --state release_oldfsm.state --glacierbin prod
+#./gemu-test.pl |USENEWFSM=0 ./gemu-test-worker.pl -n 4  --state release_oldfsm.state --glacierbin prod
+#./gemu-test.pl |USENEWFSM=1 ./gemu-test-worker.pl -n 4  --state release_newfs_mixed.state --glacierbin dev
+#./gemu-test.pl |NEWFSM=1 ./gemu-test-worker.pl -n 4  --state release_newfs_full.state --glacierbin dev
+./gemu-test.pl |NEWFSM=0 ./gemu-test-worker.pl -n 4  --state release_drop_old_fsm.state --glacierbin dev
