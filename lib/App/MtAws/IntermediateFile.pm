66c7f4c2376184ed500ce25e890b46e23227c5f4
Victor <victor@vsespb.ru>
2014-01-04 21:35:43 +0400

    copyright year updated

--- lib/App/MtAws/IntermediateFile.pm
+++ lib/App/MtAws/IntermediateFile.pm
@@ -1,5 +1,5 @@
 # mt-aws-glacier - Amazon Glacier sync client
-# Copyright (C) 2012-2013  Victor Efimov
+# Copyright (C) 2012-2014  Victor Efimov
 # http://mt-aws.com (also http://vs-dev.com) vs@vs-dev.com
 # License: GPLv3
 #
